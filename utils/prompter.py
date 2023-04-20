b6021415903c523f357a220c19ecf65f34ec6be1
Yueyue Long <ylong6@bhg0056.bluehive.circ.private>
2023-04-20 19:45:10 -0400

    new prompter

--- utils/prompter.py
+++ utils/prompter.py
@@ -14,7 +14,7 @@ class Prompter(object):
         self._verbose = verbose
         if not template_name:
             # Enforce the default here, so the constructor can be called with '' and will not break.
-            template_name = "camel"
+            template_name = "alpaca"
         file_name = osp.join("templates", f"{template_name}.json")
         if not osp.exists(file_name):
             raise ValueError(f"Can't read {file_name}")
@@ -27,25 +27,22 @@ class Prompter(object):
 
     def generate_prompt(
         self,
-        person_a: str,
-        person_b: Union[None, str] = None,
-        conversation: Union[None, str] = None,
+        instruction: str,
+        input: Union[None, str] = None,
+        label: Union[None, str] = None,
     ) -> str:
         # returns the full prompt from instruction and optional input
         # if a label (=response, =output) is provided, it's also appended.
-        # if input:
-        #     res = self.template["prompt_input"].format(
-        #         instruction=instruction, input=input
-        #     )
-        # else:
-        #     res = self.template["prompt_no_input"].format(
-        #         instruction=instruction
-        #     )
-        res = self.template["prompt_input"].format(
-            person_a=person_a,person_b=person_b
-        )
-        if conversation:
-            res = f"{res}{conversation}"
+        if input:
+            res = self.template["prompt_input"].format(
+                instruction=instruction, input=input
+            )
+        else:
+            res = self.template["prompt_no_input"].format(
+                instruction=instruction
+            )
+        if label:
+            res = f"{res}{label}"
         if self._verbose:
             print(res)
         return res
